6e76d891455ab8428daa11338e9d1017beef51b9
Prabin Giri <prabin17np@gmail.com>
2026-06-29 21:31:19 +0545

    creating user types

---
+++ backend/src/types/user.types.ts
@@ -0,0 +1,13 @@
+import z from "zod";
+
+export const UserSchema = z.object({
+  username: z.string().min(3).max(30),
+  email: z.email(),
+  password: z.string().min(8),
+  firstName : z.string().optional(),
+  lastName : z.string().optional(),
+  profilePicture: z.string().nullable().optional(),
+  role : z.enum(["user", "admin"]).default("user")
+});
+
+export type UserType = z.infer<typeof UserSchema>;
